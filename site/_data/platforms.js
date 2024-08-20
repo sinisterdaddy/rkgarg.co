f2db906b7a6ed5d0146de75042843db7bb6b462c
Radha Krishna <krishna.garg010604@gmail.com>
2024-08-20 21:24:49 +0530

    upd

--- site/_data/platforms.js
+++ site/_data/platforms.js
@@ -1,36 +1,38 @@
 module.exports = [
   {
-    id: 'egghead',
-    label: 'egghead',
-    link: 'https://jason.energy/egghead',
+    id: 'EcoMotion',
+    label: 'EcoMotion',
+    link: 'https://github.com/sinisterdaddy/EcoMotion',
     description: `
       Jason has created dozens of video lessons on egghead. These videos skip
       the chatter, instead diving straight into the code and delivering tons of
       practical knowledge a few minutes at a time.
     `,
-    image: 'egghead',
+    image: 'https://github.com/user-attachments/assets/a7d578fc-24ca-447d-96f9-67270c4b96ca',
   },
   {
-    id: 'lwj',
-    label: 'Learn With Jason',
-    link: 'https://www.learnwithjason.dev',
+    id: '3dPortfolio',
+    label: '3D Portfolio',
+    link: 'https://3-d-portfolio-teal.vercel.app/',
     description: `
-      Jason hosts <em>Learn With Jason</em> twice a week and pair programs with
-      brilliant people from community to learn something new in 90 minutes.
-      Shows are live and interactive, so 
-      <a href="https://www.learnwithjason.dev/schedule">mark your calendar</a> and join the fun!
+      created a dynamic 3D portfolio using Three.js and modern web development techniques.
+      This project showcases my work in an interactive, visually immersive environment,
+      allowing users to explore my portfolio in a unique way.
+      The site is fully responsive and hosted on Vercel for seamless performance across devices.</a> and join the fun!
     `,
-    image: 'learnwithjason',
+    image: 'https://github.com/user-attachments/assets/e4004f9f-a01e-4166-9cdc-afd213c36986',
   },
   {
-    id: 'frontendmasters',
-    label: 'Frontend Masters',
-    link: 'https://jason.energy/frontendmasters',
+    id: 'Neighbourgood',
+    label: 'Neighbourgood',
+    link: 'https://neighborgood.io/',
     description: `
-      Jason has taught several workshops with Frontend Masters, a long-form
-      workshop format focused on deep dives into a topic for a full day. These
-      workshops are packed with the “why” & “how” behind the topic.
+      NeighborGood is designed to simplify the way neighborhoods form connections and build a sense of community.
+      Our platform features an AI agent, inspired by the friendly and outgoing extrovert neighbor,
+      who identifies and suggests symbiotic activities for people to enjoy together.
+      Built with React, HTML, and JavaScript on the frontend, and powered by a Django backend with OpenAI techniques,
+      NeighborGood is a seamless and intelligent solution for fostering neighborhood engagement.
     `,
-    image: 'frontendmasters',
+    image: 'https://github.com/user-attachments/assets/ee1fd7b4-6a45-4ff2-a1d2-c09863200c80',
   },
 ];
