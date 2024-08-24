57883d9c1d0f49cbfd9f2bc706bb2cf7a1c02bc9
Radha Krishna <krishna.garg010604@gmail.com>
2024-08-24 15:19:00 +0530

    updates final

--- site/_data/platforms.js
+++ site/_data/platforms.js
@@ -1,14 +1,15 @@
 module.exports = [
   {
-    id: 'EcoMotion',
-    label: 'EcoMotion',
-    link: 'https://github.com/sinisterdaddy/EcoMotion',
+    id: 'GeoQuest',
+    label: 'GeoQuest',
+    link: 'https://github.com/sinisterdaddy/GeoQuest-Precision-Pursuit',
     description: `
-      Jason has created dozens of video lessons on egghead. These videos skip
-      the chatter, instead diving straight into the code and delivering tons of
-      practical knowledge a few minutes at a time.
+      Test your geographical skills in this exciting multiplayer game where you guess real-world locations on a map.
+      Built with Node.js and React, "GeoQuest: Precision Pursuit" challenges you to pinpoint locations as accurately as possible
+      to earn points.
+      Compete with friends or players worldwide and climb the leaderboards in this fast-paced geolocation guessing game.
     `,
-    image: 'https://github.com/user-attachments/assets/a7d578fc-24ca-447d-96f9-67270c4b96ca',
+    image: 'https://github.com/user-attachments/assets/505e581c-6111-4456-a19e-bff1e6936d82',
   },
   {
     id: '3dPortfolio',
@@ -20,7 +21,7 @@ module.exports = [
       allowing users to explore my portfolio in a unique way.
       The site is fully responsive and hosted on Vercel for seamless performance across devices.</a> and join the fun!
     `,
-    image: 'https://github.com/user-attachments/assets/e4004f9f-a01e-4166-9cdc-afd213c36986',
+    image: 'https://github.com/user-attachments/assets/29a14099-5d64-44b7-ac26-afdab93926e8',
   },
   {
     id: 'Neighbourgood',
@@ -30,8 +31,6 @@ module.exports = [
       NeighborGood is designed to simplify the way neighborhoods form connections and build a sense of community.
       Our platform features an AI agent, inspired by the friendly and outgoing extrovert neighbor,
       who identifies and suggests symbiotic activities for people to enjoy together.
-      Built with React, HTML, and JavaScript on the frontend, and powered by a Django backend with OpenAI techniques,
-      NeighborGood is a seamless and intelligent solution for fostering neighborhood engagement.
     `,
     image: 'https://github.com/user-attachments/assets/ee1fd7b4-6a45-4ff2-a1d2-c09863200c80',
   },
